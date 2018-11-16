9ead588c92fdea6be3e4cd5e85ced0139e0c2e0b
SaraBon <sarabongartz123@gmail.com>
2018-11-16 17:08:51 +0100

    Updates

---
+++ precache-manifest.00a75adb2dba3804617ba1bd9ca07679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ed1dc90a2bacf315f9c",
+    "url": "/minesweeper-react/static/js/runtime~main.9ed1dc90.js"
+  },
+  {
+    "revision": "1f5e9afbd99c6addde9d",
+    "url": "/minesweeper-react/static/js/main.1f5e9afb.chunk.js"
+  },
+  {
+    "revision": "2dddc04e7f91615d182a",
+    "url": "/minesweeper-react/static/js/1.2dddc04e.chunk.js"
+  },
+  {
+    "revision": "1f5e9afbd99c6addde9d",
+    "url": "/minesweeper-react/static/css/main.f231dabb.chunk.css"
+  },
+  {
+    "revision": "ba30a4d5a80198fa2af9db7b54dfb127",
+    "url": "/minesweeper-react/index.html"
+  }
+];
